762db43d76441960fbdca771282e3074c64cbb2b
2573997834 <2573997834@qq.com>
2022-08-22 15:24:48 +0800

    test

--- src/main/java/com/lym/testdev/TestdevApplication.java
+++ src/main/java/com/lym/testdev/TestdevApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(scanBasePackages = {"com.oms.testdev"}, exclude = {DruidDataSourceAutoConfigure.class})
-@MapperScan(value = {"com.oms.testdev.dao"})
+@SpringBootApplication(scanBasePackages = {"com.lym.testdev"}, exclude = {DruidDataSourceAutoConfigure.class})
+@MapperScan(value = {"com.lym.testdev.dao"})
 public class TestdevApplication {
 
     public static void main(String[] args) {
